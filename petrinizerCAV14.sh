5a4b4139219386a86e325e48954b592c105dcfe8
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2022-05-20 16:07:16 +0200

    version 1 : treating CAV'14 benchmark

--- petrinizerCAV14.sh
+++ petrinizerCAV14.sh
@@ -8,22 +8,22 @@ cd petrinizer
 # step 1 : download the benchmarks from git
 # this is as far as we know the latest repo used by Petrinizer author Meyer
 wget https://gitlab.lrz.de/i7/petrinizer/-/archive/master/petrinizer-master.tar.gz
-tar xvzf petrinizer-master.tar.gz
+tar xzf petrinizer-master.tar.gz
 
 # sequence for CAV'14
 mkdir -p $root/CAV14
 mkdir -p $root/CAV14/oracle
 
-cd benchmarks/cav-benchmarks
+cd petrinizer-master/benchmarks/cav-benchmarks
 
 # each model is presented as a spec file with a reachability goal	
 for i in $(find -name *.spec -print) ; 
 do 
 	# a unique name for the model instance
-	j=$(echo $i | sed 's/\.\///' | sed 's#/#-#g' | sed 's/.spec//g') ; 
+	j=$(echo $i | sed 's/\.\///' | sed 's#/#_#g' | sed 's#__#_#g' | sed 's/.spec//g')  
 	mkdir -p $j ; 
 	cd $j ; 
-	$root/spec2pnml.sh ../$i ;
+    $root/itstools/its-tools -convert ../$i -o ./
 	# trace to source
 	cp ../$i . 
 	cd .. ;
