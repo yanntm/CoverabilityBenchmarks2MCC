a35037d6f987eec33fa39c9afb7b14971896f724
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2022-05-20 16:24:43 +0200

    dont double download

--- petrinizerCAV14.sh
+++ petrinizerCAV14.sh
@@ -5,10 +5,14 @@ export root=$PWD
 mkdir -p petrinizer
 cd petrinizer
 
-# step 1 : download the benchmarks from git
-# this is as far as we know the latest repo used by Petrinizer author Meyer
-wget https://gitlab.lrz.de/i7/petrinizer/-/archive/master/petrinizer-master.tar.gz
-tar xzf petrinizer-master.tar.gz
+
+if [ ! -f petrinizer-master.tar.gz ] ;
+	then
+	# step 1 : download the benchmarks from git
+	# this is as far as we know the latest repo used by Petrinizer author Meyer
+	wget https://gitlab.lrz.de/i7/petrinizer/-/archive/master/petrinizer-master.tar.gz
+	tar xzf petrinizer-master.tar.gz
+	fi
 
 # sequence for CAV'14
 mkdir -p $root/CAV14
